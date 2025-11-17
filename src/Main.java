bbe501e0c17448956fdacb2dc27c8073b8005a14
AlexanderChen <hello@gmail.com>
2025-11-17 14:42:44 -0500

    Added basics to inventory and using items

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,11 @@ import java.util.Scanner;
 
 /*
     TO DO:
-    Main tasks
+    1. Add fishing
+    2. Fix inventory
+    3. Make stats work with scalable luck
+    4. Add fish
+    5. Useable items, maybe selling fish
 
 
 
@@ -14,25 +18,40 @@ public class Main {
         Boolean Playing = true;
         String spacer = "-------------------------------------------------------------------------------";
         Fishiest user = new Fishiest();
+        int screen = 1;
 
 
         System.out.println("Welcome to Fishiest, the worlds best fishing game in the command line");
         while (Playing) {
-            System.out.println(spacer);
-            System.out.println("Hello what would you like to do \n1. Cast lure \n2. Access items\n3. blah blah");
-            int inp = _scan.nextInt();
+            if (screen == 1) {
+                System.out.println(spacer);
+                System.out.println("Hello what would you like to do \n1. Cast lure \n2. Access items\n3. blah blah");
+                int inp = _scan.nextInt();
 
-            if (inp == 1) {
-                System.out.println(spacer);
-                System.out.println("you casted a lure");
-            } else if (inp == 2) {
-                System.out.println(spacer);
+                if (inp == 1) {
+                    System.out.println(spacer);
+                    System.out.println("you casted a lure");
+                } else if (inp == 2) {
+                    screen = 2;
+                }
+
+
+                if (inp == 0) {
+                    Playing = false;
+                }
+            } else if (screen == 2) {
+                //Accessing inventory
                 user.inventoryCheck();
-            }
+                System.out.println(spacer);
+                System.out.println("If you wish to use any, please type the index of item, else type 0 to exit");
+                int inp = _scan.nextInt();
 
-
-            if (inp == 0) {
-                Playing = false;
+                if (inp == 0) {
+                    screen = 1;
+                } else {
+                    user.useItem(inp);
+                    screen = 1;
+                }
             }
         }
     }
