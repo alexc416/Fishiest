ff47876da2bac8d33eac7b9479385e5d236af0f0
alexc416 <233334840+alexc416@users.noreply.github.com>
2025-11-18 21:44:48 -0500

    Added basics to fishing mechanics with reeling and giving line, need to balance it but its a good start

--- src/Main.java
+++ src/Main.java
@@ -8,7 +8,6 @@ import java.util.Scanner;
     4. Add fish
     5. Useable items, maybe selling fish
 
-    //test
     Also clean up UI
 
 
@@ -32,6 +31,9 @@ public class Main {
 
                 if (inp == 1) {
                     screen = 2;
+                    System.out.println(spacer);
+                    System.out.println("You have a fish on the line, do you wish to \n1. Pull it out of the water! \n2. Start reeling it in \n3. Give it some more line");
+                    System.out.println("(HINT!: Giving the fish line is always a safe option, you want to have a balence between reeling and giving, untill finaly pulling it out of the water when you think you closed the distance, yet also have the fish intrested)");
                 } else if (inp == 2) {
                     screen = 3;
                 }
@@ -41,24 +43,49 @@ public class Main {
                 }
 
             } else if (screen == 2) {
-                System.out.println(spacer);
-                int fish = user.catchFish();
-                int difficulty = 1;
+                int fishCaught = user.catchFish();
+
+                int difficulty = 5;
+                //difficulty is going to be based on difficulty of fish and affects the necessary fish attraction and range 3 5 7 10
+                int distance = (int) (Math.random()*10)+5;
+                int fishAtraction = 1;
                 boolean catching = true;
-                int distance = (int) (Math.random() * (difficulty * 100));
                 //placeholder for now, until adding luck
-                System.out.println("You have a fish on the line at distance " + distance + " do you wish to \n1. Pull it out of the water! \n2. Start reeling it in \n3. Give it some more line");
-                if (catching) {
+
+                //1 is pull out, 2 is reel in, 3 is give line
+                while (catching) {
                     int inp = _scan.nextInt();
+                    if (inp == 1) {
+                        if (fishAtraction > difficulty && distance < 10) {
+                            System.out.println("You pulled the fish out of the water! Congrats you caught a " + fishCaught);
+                        } else {
+                            if (distance >= 10) {
+                                System.out.println("The fish is was too far away!"); 
+                            } else {
+                                System.out.println("You reeled in the fish to early!");
+                            }
+                        }
+                        catching = false;
+                    } else if (inp == 2) {
+                        int reelRoll = (int) (Math.random()*26);
+                        if (reelRoll > difficulty) {
+                            System.out.println("You reeled in the fish closer.");
+                            distance -= 5;
+                            fishAtraction += 2;
+                        } else {
+                            System.out.println("The fish pulled away and let go!");
+                            catching = false;
+                        }
 
+                    } else if (inp == 3) {
+                        System.out.println("You gave the fish some more line.");
+                        fishAtraction += 2;
+                        distance += 3;
+                    }
 
-
-
+                    System.out.println("What do you wish to do next? \n1. Pull it out of the water! \n2. Start reeling it in \n3. Give it some more line");
+                    System.out.println();
                 }
-
-
-
-
 
             } else if (screen == 3) {
                 //Accessing inventory
@@ -74,5 +101,6 @@ public class Main {
                 }
             }
         }
+        _scan.close();
     }
 }
